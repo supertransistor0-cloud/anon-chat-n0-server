4c98666c1b48b59fe457ba65dcc10b92c1676ce0
supertransistor0-cloud <supertransistor0@gmail.com>
2026-04-09 11:06:14 +0300

    Server

---
+++ server.js
@@ -0,0 +1,31 @@
+// Токен и гист
+var github_token   =   '' ;
+var gist_id        =   '' ;
+var max_length     =   500;
+
+
+// Отправка и чтение
+function send(sd) {
+    /* Отправка файла на сервер (Гист)
+     * С зашифровкой
+    */
+}
+
+function read() {
+    /* Чтение файла с сервера (Гист)
+     * С расшифровкой
+    */
+    return rd;
+}
+
+
+// Очистка
+function check_len() {
+    text = read()
+    if (text.length() == max_length) {
+        send("0")
+    }
+}
+
+
+// Нужно добавить функцию для токенов
